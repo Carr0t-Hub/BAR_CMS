6e50cde3367573a65a2165004472d5e5ed086953
Paul Osorio <106576044+paul-osorio@users.noreply.github.com>
2024-06-25 14:53:00 +0800

    chore: Update international partners page layout and display

--- partners/international.php
+++ partners/international.php
@@ -2,14 +2,46 @@
 require_once("../common/config.php");
 
 $title = "INTERNATIONAL PARTNERS";
+$partners  = getInternationalPartner($mysqli);
 
 ob_start();
 ?>
 
-<div class="thumbnail-container">
-    <div class="thumbnail">
-        <iframe src="http://afaci.org/main" frameborder="0" onload="var that=this;setTimeout(function() { that.style.opacity = 1 }, 500)"></iframe>
-    </div>
+<div class="row gy-3">
+    <?php
+    foreach ($partners as $partner) {
+
+        $img = $partner['fileName'] . '_' . $partner['size'] . $partner['thumbnail'] . '.' . $partner['fileExtension'];
+
+
+    ?>
+        <!-- <div class="col-lg-3 col-md-6">
+            <div class="">
+                <img src="" alt="" class="rounded" style="height: 320px;width: 100%;object-fit: cover">
+
+
+
+            </div>
+
+        </div> -->
+
+        <div class="col-lg-3 col-md-6">
+            <div class="d-items">
+                <div class="card-image-1 de-offer" style="background-color: black;">
+                    <a href=" <?= $partner['link'] ?>" class="d-text">
+                        <div class="d-inner" style="background:rgba(0,0,0,0.6); ">
+                            <div class="p-2" style="font-size: 18px"><?= $partner['name'] ?></div>
+
+                        </div>
+                    </a>
+                    <img src="../admin/storage/files/partners/<?= $img ?>" class="img-fluid" alt="">
+                </div>
+            </div>
+        </div>
+    <?php
+
+    }
+    ?>
 </div>
 
 <?php
