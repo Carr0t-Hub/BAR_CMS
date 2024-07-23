9196651f39406f52a254cf037aaf1151f5591cc4
Mayrene Gail <113238880+MayreneGail@users.noreply.github.com>
2024-07-23 14:00:23 +0800

    Added context

--- chronicles.php
+++ chronicles.php
@@ -1,160 +1,160 @@
 <?php include("common/header.php"); ?>
 <div id="background" data-bgimage="url(images/background/9.jpg) fixed"></div>
-  <div id="content-absolute">
-    <section id="subheader" class="no-bg">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12 text-center">
-            <h4>Latest</h4>
-            <h1>BAR Chronicles</h1>
-          </div>
+<div id="content-absolute">
+  <section id="subheader" class="no-bg">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 text-center">
+          <h4>Latest</h4>
+          <h1>BAR Chronicles</h1>
         </div>
       </div>
-    </section>
+    </div>
+  </section>
 
-    <section id="section-main" class="no-bg no-top" aria-label="section-menu">
-      <div class="container">
-        <div class="row g-4">
-          <div class="col-lg-4">
-            <div class="de-room">
-              <div class="d-image">
-                <div class="d-details">
-                  <span class="d-meta-1">
-                    <img src="images/ui/user.svg" alt="">2024
-                  </span>
-                  <!-- <span class="d-meta-2">
+  <section id="section-main" class="no-bg no-top" aria-label="section-menu">
+    <div class="container">
+      <div class="row g-4">
+        <div class="col-lg-4">
+          <div class="de-room">
+            <div class="d-image">
+              <div class="d-details">
+                <span class="d-meta-1">
+                  <img src="images/ui/user.svg" alt="">2024
+                </span>
+                <!-- <span class="d-meta-2">
                     <img src="images/ui/floorplan.svg" alt="">30 ft
                   </span> -->
-                </div>
-                <a href="#">
-                  <img src="images/room/1.jpg" class="img-fluid" alt="">
-                  <img src="images/room/1-alt.jpg" class="d-img-hover img-fluid" alt="">
-                </a>
               </div>
-              <div class="d-text">
-                <h3>Standard Room</h3>
-                <p>Most hotels and major hospitality companies have set industry standards to classify hotel types. An upscale full-service hotel facility offers luxury...</p>
-                <a href="#" class="btn-line"><span>View More</span></a>
-              </div>
+              <a href="#">
+                <img src="images/room/1.jpg" class="img-fluid" alt="">
+                <img src="images/room/1-alt.jpg" class="d-img-hover img-fluid" alt="">
+              </a>
             </div>
-          </div>
-          <div class="col-lg-4">
-            <div class="de-room">
-              <div class="d-image">
-                <div class="d-details">
-                  <span class="d-meta-1">
-                    <img src="images/ui/user.svg" alt="">2024
-                  </span>
-                  <!-- <span class="d-meta-2">
-                    <img src="images/ui/floorplan.svg" alt="">30 ft
-                  </span> -->
-                </div>
-                <a href="#">
-                  <img src="images/room/2.jpg" class="img-fluid" alt="">
-                  <img src="images/room/2-alt.jpg" class="d-img-hover img-fluid" alt="">
-                </a>
-              </div>
-              <div class="d-text">
-                <h3>Standard Room</h3>
-                <p>Most hotels and major hospitality companies have set industry standards to classify hotel types. An upscale full-service hotel facility offers luxury...</p>
-                <a href="#" class="btn-line"><span>View More</span></a>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-4">
-            <div class="de-room">
-              <div class="d-image">
-                <div class="d-details">
-                  <span class="d-meta-1">
-                    <img src="images/ui/user.svg" alt="">2024
-                  </span>
-                  <!-- <span class="d-meta-2">
-                    <img src="images/ui/floorplan.svg" alt="">30 ft
-                  </span> -->
-                </div>
-                <a href="#">
-                  <img src="images/room/3.jpg" class="img-fluid" alt="">
-                  <img src="images/room/3-alt.jpg" class="d-img-hover img-fluid" alt="">
-                </a>
-              </div>
-              <div class="d-text">
-                <h3>Standard Room</h3>
-                <p>Most hotels and major hospitality companies have set industry standards to classify hotel types. An upscale full-service hotel facility offers luxury...</p>
-                <a href="#" class="btn-line"><span>View More</span></a>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-4">
-            <div class="de-room">
-              <div class="d-image">
-                <div class="d-details">
-                  <span class="d-meta-1">
-                    <img src="images/ui/user.svg" alt="">2024
-                  </span>
-                  <!-- <span class="d-meta-2">
-                    <img src="images/ui/floorplan.svg" alt="">30 ft
-                  </span> -->
-                </div>
-                <a href="#">
-                  <img src="images/room/4.jpg" class="img-fluid" alt="">
-                  <img src="images/room/4-alt.jpg" class="d-img-hover img-fluid" alt="">
-                </a>
-              </div>
-              <div class="d-text">
-                <h3>Standard Room</h3>
-                <p>Most hotels and major hospitality companies have set industry standards to classify hotel types. An upscale full-service hotel facility offers luxury...</p>
-                <a href="#" class="btn-line"><span>View More</span></a>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-4">
-            <div class="de-room">
-              <div class="d-image">
-                <div class="d-details">
-                  <span class="d-meta-1">
-                    <img src="images/ui/user.svg" alt="">2024
-                  </span>
-                  <!-- <span class="d-meta-2">
-                    <img src="images/ui/floorplan.svg" alt="">30 ft
-                  </span> -->
-                </div>
-                <a href="#">
-                  <img src="images/room/5.jpg" class="img-fluid" alt="">
-                  <img src="images/room/5-alt.jpg" class="d-img-hover img-fluid" alt="">
-                </a>
-              </div>
-              <div class="d-text">
-                <h3>Standard Room</h3>
-                <p>Most hotels and major hospitality companies have set industry standards to classify hotel types. An upscale full-service hotel facility offers luxury...</p>
-                <a href="#" class="btn-line"><span>View More</span></a>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-4">
-            <div class="de-room">
-              <div class="d-image">
-                <div class="d-details">
-                  <span class="d-meta-1">
-                    <img src="images/ui/user.svg" alt="">2024
-                  </span>
-                  <!-- <span class="d-meta-2">
-                    <img src="images/ui/floorplan.svg" alt="">30 ft
-                  </span> -->
-                </div>
-                <a href="#">
-                  <img src="images/room/6.jpg" class="img-fluid" alt="">
-                  <img src="images/room/6-alt.jpg" class="d-img-hover img-fluid" alt="">
-                </a>
-              </div>
-              <div class="d-text">
-                <h3>Standard Room</h3>
-                <p>Most hotels and major hospitality companies have set industry standards to classify hotel types. An upscale full-service hotel facility offers luxury...</p>
-                <a href="#" class="btn-line"><span>View More</span></a>
-              </div>
+            <div class="d-text">
+              <h3>Standard Room</h3>
+              <p>Most hotels and major hospitality companies have set industry standards to classify hotel types. An upscale full-service hotel facility offers luxury...</p>
+              <a href="#" class="btn-line"><span>View More</span></a>
             </div>
           </div>
         </div>
+        <div class="col-lg-4">
+          <div class="de-room">
+            <div class="d-image">
+              <div class="d-details">
+                <span class="d-meta-1">
+                  <img src="images/ui/user.svg" alt="">2024
+                </span>
+                <!-- <span class="d-meta-2">
+                    <img src="images/ui/floorplan.svg" alt="">30 ft
+                  </span> -->
+              </div>
+              <a href="#">
+                <img src="images/room/2.jpg" class="img-fluid" alt="">
+                <img src="images/room/2-alt.jpg" class="d-img-hover img-fluid" alt="">
+              </a>
+            </div>
+            <div class="d-text">
+              <h3>Standard Room</h3>
+              <p>Most hotels and major hospitality companies have set industry standards to classify hotel types. An upscale full-service hotel facility offers luxury...</p>
+              <a href="#" class="btn-line"><span>View More</span></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="de-room">
+            <div class="d-image">
+              <div class="d-details">
+                <span class="d-meta-1">
+                  <img src="images/ui/user.svg" alt="">2024
+                </span>
+                <!-- <span class="d-meta-2">
+                    <img src="images/ui/floorplan.svg" alt="">30 ft
+                  </span> -->
+              </div>
+              <a href="#">
+                <img src="images/room/3.jpg" class="img-fluid" alt="">
+                <img src="images/room/3-alt.jpg" class="d-img-hover img-fluid" alt="">
+              </a>
+            </div>
+            <div class="d-text">
+              <h3>Standard Room</h3>
+              <p>Most hotels and major hospitality companies have set industry standards to classify hotel types. An upscale full-service hotel facility offers luxury...</p>
+              <a href="#" class="btn-line"><span>View More</span></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="de-room">
+            <div class="d-image">
+              <div class="d-details">
+                <span class="d-meta-1">
+                  <img src="images/ui/user.svg" alt="">2024
+                </span>
+                <!-- <span class="d-meta-2">
+                    <img src="images/ui/floorplan.svg" alt="">30 ft
+                  </span> -->
+              </div>
+              <a href="#">
+                <img src="images/room/4.jpg" class="img-fluid" alt="">
+                <img src="images/room/4-alt.jpg" class="d-img-hover img-fluid" alt="">
+              </a>
+            </div>
+            <div class="d-text">
+              <h3>Standard Room</h3>
+              <p>Most hotels and major hospitality companies have set industry standards to classify hotel types. An upscale full-service hotel facility offers luxury...</p>
+              <a href="#" class="btn-line"><span>View More</span></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="de-room">
+            <div class="d-image">
+              <div class="d-details">
+                <span class="d-meta-1">
+                  <img src="images/ui/user.svg" alt="">2024
+                </span>
+                <!-- <span class="d-meta-2">
+                    <img src="images/ui/floorplan.svg" alt="">30 ft
+                  </span> -->
+              </div>
+              <a href="#">
+                <img src="images/room/5.jpg" class="img-fluid" alt="">
+                <img src="images/room/5-alt.jpg" class="d-img-hover img-fluid" alt="">
+              </a>
+            </div>
+            <div class="d-text">
+              <h3>Standard Room</h3>
+              <p>Most hotels and major hospitality companies have set industry standards to classify hotel types. An upscale full-service hotel facility offers luxury...</p>
+              <a href="#" class="btn-line"><span>View More</span></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="de-room">
+            <div class="d-image">
+              <div class="d-details">
+                <span class="d-meta-1">
+                  <img src="images/ui/user.svg" alt="">2024
+                </span>
+                <!-- <span class="d-meta-2">
+                    <img src="images/ui/floorplan.svg" alt="">30 ft
+                  </span> -->
+              </div>
+              <a href="#">
+                <img src="images/room/6.jpg" class="img-fluid" alt="">
+                <img src="images/room/6-alt.jpg" class="d-img-hover img-fluid" alt="">
+              </a>
+            </div>
+            <div class="d-text">
+              <h3>Standard Room</h3>
+              <p>Most hotels and major hospitality companies have set industry standards to classify hotel types. An upscale full-service hotel facility offers luxury...</p>
+              <a href="#" class="btn-line"><span>View More</span></a>
+            </div>
+          </div>
+        </div>
       </div>
-    </section>
+    </div>
+  </section>
 
-<?php include("common/footer.php"); ?>
+  <?php include("common/footer.php"); ?>
